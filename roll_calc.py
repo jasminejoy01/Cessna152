d3fd9bacbfd0b7cfcd186531ea2c80336da6cdde
jasminejoy01 <55416635+jasminejoy01@users.noreply.github.com>
2021-11-12 14:26:34 -0500

    Add files via upload

--- roll_calc.py
+++ roll_calc.py
@@ -10,6 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import weather
 
+import re
+
+'''User Input '''
+# 'CFC8' - Flamboro Centre
+# 'CYHM' - Hamilton
+airport = 'CFC8'
+
 
 taf = '''TAF AMD CYHM 120831Z 1208/1306 26012G22KT P6SM BKN040 BKN080 TEMPO
 1208/1210 5SM BR BKN008 OVC012 PROB30 1208/1210 2SM -DZ BR
@@ -27,50 +34,54 @@ FM121000 25012G22KT P6SM SCT020
 FM121300 24012G22KT P6SM FEW025
 '''
 
-#PA = 723.0
-#T = 10
-#wind = 10
-#gust = 20
-turf = 'soft'
-rwy = np.array([33, 15])
-ground_elevation = 813
-variation = 10
-
-weather.note_Z(taf, 0)
-
-altimeter = (int(weather.altimeter(metar))/100)
-PA = round(ground_elevation + ((29.92 - altimeter)*1000),2)
-print("PA: ", PA,"\n")
-
-knots = weather.knots_KT(string)
-direction = int(knots[0])
-wind = int(knots[1])
-gust = int(knots[-1])
-magnetic_dir = direction+variation
-print("Magnetic: ", magnetic_dir, wind, gust)
-
-rwy_calc = np.abs(rwy*10 - magnetic_dir)
-print(rwy*10, rwy_calc, min(rwy_calc))
-
-temp_dewpt = weather.temperature(metar)
-print(temp_dewpt, "\n")
-T = int(temp_dewpt[0])
-
-
 # Import CSV
 gr_ = pd.read_csv('TO_ground_roll_PA.txt', delimiter="\t")
 gr50_ = pd.read_csv('TO_50feet_PA.txt', delimiter="\t")
 land_ = pd.read_csv('Landing_ground_roll_PA.txt', delimiter="\t")
 land50_ = pd.read_csv('Landing_50feet_PA.txt', delimiter="\t")
+aerodomes = pd.read_csv('aerodomes.txt', delimiter="\t")
 
 # Create Dataframes
 df_gr = pd.DataFrame(gr_)
 df50_gr = pd.DataFrame(gr50_)
 df_land = pd.DataFrame(land_)
 df50_landgr = pd.DataFrame(land50_)
+df_aerodomes = pd.DataFrame(aerodomes)
+
+''' Aerodomes '''
+def _airport(name):
+    for index, row in df_aerodomes.iterrows():
+        if row['aerodome'] == name:
+            print( row['Description'])
+            rwy = (row['Rwys'])
+            ground_elev = row['ground_elevation_ft']
+            var = row['variation']
+            var_dir = row['var_dir']
+            rwy_disp = (row['Displacement'])
+            rwy_len = (row['RwyLength'])
+            if row['RwyType'] != 'asphalt':
+                rwy_type = 'soft'
+            else:
+                rwy_type = 'asphalt'
+    print(rwy, ground_elev, var, var_dir, rwy_type, rwy_disp, rwy_len, "\n")
+    return rwy, ground_elev, var, var_dir, rwy_type, rwy_disp, rwy_len
+
+#aerodome	ground_elevation_ft	variation	var_dir	Description	RwyType	Rwys	Displacement	RwyLength
+aerodome_values = _airport(airport)
+ground_elevation = int(aerodome_values[1])
+turf = aerodome_values[4]
+rwys = aerodome_values[0]
+rwy_len = aerodome_values[-1]
+rwy_disp = aerodome_values[-2]
+if aerodome_values[3] == 'W':
+    variation = round(int(aerodome_values[2]), 2)
+elif aerodome_values[3] == 'E':
+    variation = -1*round(int(aerodome_values[2]), 2)
+
+#print(ground_elevation, turf)
 
 class Calculation:
-    def __init__(self, pa = PA,  temperature = T, df = None):
+    def __init__(self, pa = None,  temperature = None, df = None):
         self.pa = pa
         self.max_val = df.shape[-1]
         self.temperature = temperature
@@ -125,7 +136,7 @@ class WindCorrection:
             factor = (9/100)*self.wind
             #print(self.windtype, self.wind, self.gust, self.groundroll)
             #factor = self.wind*factor 
-            #print(self.groundroll - factor)
+            #print(self.groundroll*factor)
             return self.groundroll*factor
     
 class Calculations50:
@@ -134,6 +145,7 @@ class Calculations50:
         self.turf = turf
         self.leg = leg
         self.windcorrected = windcorrected
+        #print(self.groundroll, self.turf ,   self.leg,   self.windcorrected )
         
     def turf_factor(self):
         if self.turf != "asphalt":
@@ -147,10 +159,75 @@ class Calculations50:
                 turf_correction = factor + self.windcorrected
                 #print(turf_correction)
         else:
-            turf_correction = 0
+            factor = 0
+            turf_correction = factor + self.windcorrected
         return turf_correction
     
+#PA = 723.0
+#T = 10
+#wind = 10
+#gust = 20
+#ground_elevation = 813
+#variation = 10
+
+rwy = np.zeros(len(rwys.split()))
+i = 0
+for each in rwys.split():
+    each = each.replace('[', '')
+    each = each.replace(']', '')
+    each = each.replace(',', '')
+    rwy[i] = int(each)
+    i = i + 1
+
+rwylen = np.zeros(len(rwy_len.split()))
+i = 0
+for each in rwy_len.split():
+    #print(each)
+    each = each.replace('[', '')
+    each = each.replace(']', '')
+    each = each.replace(',', '')
+    rwylen[i] = int(each)
+    i = i + 1
+
+rwydisplacement = np.zeros(len(rwy_len.split()))
+i = 0
+for each in rwy_disp.split():
+    #print(each)
+    #each = each.replace(each,"[" )
+    each = each.replace('[', '')
+    each = each.replace(']', '')
+    each = each.replace(',', '')
+    rwydisplacement[i] = int(each)
+    i = i + 1
+
+weather.note_Z(taf, 0)
+
+altimeter = (int(weather.altimeter(metar))/100)
+PA = round(ground_elevation + ((29.92 - altimeter)*1000),2)
+print("PA: ", PA,"\n")
+
+knots = weather.knots_KT(string)
+direction = int(knots[0])
+wind = int(knots[1])
+gust = int(knots[-1])
+magnetic_dir = direction+variation
+print("Magnetic: ", magnetic_dir, wind, gust)
+
+rwy_calc = np.abs(rwy*10 - magnetic_dir)
+print(rwy*10, rwy_calc, min(rwy_calc))
+
+result = np.where(rwy_calc == min(rwy_calc))
+idx = (result[0][0])
+print(rwy[idx])
+
+temp_dewpt = weather.temperature(metar)
+print(temp_dewpt, "\n")
+T = int(temp_dewpt[0])
+#print(T)
+
 ''' T.O. Ground Roll '''
+print("Use Rwy {0} of length {1} with displacement of {2} \n".format(rwy[idx],rwylen[idx], rwydisplacement[idx]))
+
 TO_ground_roll = Calculation(PA, T, df_gr)
 TO_groundroll = round(TO_ground_roll.calculate(), 0)
 
@@ -197,4 +274,6 @@ Land50_Calculation = round(Land50_Calculation.turf_factor(), 0)
 "Output"
 print("TO Ground Roll: {0} \nTO 50': {1} ".format(TO_Calculation50, TO50_Calculation)) 
 print("Landing Ground Roll: {0} \nLanding 50': {1} ".format(Land_Calculation50, Land50_Calculation)) 
+
+
 
